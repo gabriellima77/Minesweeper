b01e517b5b19dce83d888f045bec5801f41921ab
Gabriel Lima <gabrielpk1@live.com>
2020-11-24 19:47:39 -0300

    Add function to open area

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const btns = document.querySelectorAll('button');
 const container = document.querySelector('#container');
-let area;
 let firstBlock;
 let size;
 let mines;
@@ -19,7 +18,17 @@ function changeDisplay(para){
     }
 }
 
-function move(){
+function putFlag(e){
+    if(e.which === 3){
+        this.style.backgroundImage = 'url("https://www.flaticon.com/svg/static/icons/svg/608/608797.svg")';
+        this.style.backgroundSize = '50%';
+        this.style.backgroundRepeat = 'no-repeat';
+        this.style.backgroundPosition = 'center';
+        this.removeEventListener('click', click);
+    }
+}
+
+function click(){
     let area = Array.from(document.querySelectorAll('.tile'));
     let index = area.indexOf(this);
     let para = this.children[0];
@@ -32,40 +41,74 @@ function move(){
     if(camp[index] === 0){
         clickZero(index);
     }
+    else if(camp[index] === -1){
+        isBomb(index);
+    }
     this.classList.add('open');
 }
 
 
-function getOpenArea(index){
-    if(index[0] < 0 || index[0] > (size[0] - 1) || index[1] < 0 || index[1] > (size[1] - 1)){
+function openArea(index){
+    let tiles = document.querySelectorAll('.tile');
+    if(index[0] < 0 || index[0] >= size[1] || index[1] < 0 || index[1] >= size[0]){
+        console.log(1);
         return;
     }
-    else if(camp[index[0] * size[0] + index[1]] != 0){
-        area[index[0] * size[0] + index[1]].classList.add('open');
-        area[index[0] * size[0] + index[1]].children[0].style.display = 'initial';
+    if(tiles[index[0] * size[0] + index[1]].classList.contains('open')){
+        console.log(2);
         return;
     }
-    else if(camp[index[0] * size[0] + index[1]] == 0){
-        area[index[0] * size[0] + index[1]].classList.add('open');
+    if(camp[index[0] * size[0] + index[1]] > 0){
+        console.log(3);
+        tiles[index[0] * size[0] + index[1]].classList.add('open');
+        tiles[index[0] * size[0] + index[1]].children[0].style.display = 'initial';
+        return;
     }
-    getOpenArea([index[0], (index[1] + 1)]);
+    tiles[index[0] * size[0] + index[1]].classList.add('open');
+
+    // X and Y
+
+    openArea([index[0], (index[1] + 1)]);
+    openArea([index[0], (index[1] - 1)]);
+    openArea([(index[0] + 1), index[1]]);
+    openArea([(index[0] - 1), index[1]]);
+
+    // Diagonals
+
+    openArea([(index[0] - 1), (index[1] + 1)]);
+    openArea([(index[0] - 1), (index[1] - 1)]);
+    openArea([(index[0] + 1), (index[1] + 1)]);
+    openArea([(index[0] + 1), (index[1] - 1)]);
 }
 
 function clickZero(index){
-    area = Array.from(document.querySelectorAll('.tile'));
     let i = Math.floor(index / size[0]);
-    let j = index % size[1];
-    getOpenArea([i, j]);
+    let j = index % size[0];
+    openArea([i, j]);
 }
 
 function isBomb(){
-
+    let tiles = Array.from(document.querySelectorAll('.tile'));
+    tiles.forEach(tile => {
+        tile.removeEventListener('click', click);
+        tile.addEventListener('mousedown', putFlag);
+    });
+    
+    let h1 = document.createElement('h1');
+    h1.style.position = 'absolute';
+    h1.textContent = 'You lose!';
+    h1.style.width = `${container.offsetWidth}px`;
+    h1.style.left = `${container.offsetLeft}`;
+    h1.style.top = '45vh';
+    h1.style.backgroundColor = 'rgba(255, 0, 0, 0.5)';
+    h1.style.textAlign = 'center';
+    container.appendChild(h1);
 }
 
 function increaseNumber(row, column){
     for(let i = row-1; i <= row + 1; i++){
         for(let j = column - 1; j <= column + 1; j++){
-            if(i > -1 && i < size[0] && j > -1 && j < size[1]){
+            if(i > -1 && i < size[1] && j > -1 && j < size[0]){
                 if(camp[i * size[0] + j] === -1){
                     camp[row * size[0] + column]++;
                 }
@@ -75,8 +118,8 @@ function increaseNumber(row, column){
 }
 
 function contBomb(){
-    for(let i = 0; i < size[0]; i++){
-        for(let j = 0; j < size[1]; j++){
+    for(let i = 0; i < size[1]; i++){
+        for(let j = 0; j < size[0]; j++){
             if(camp[i * size[0] + j] !== -1){
                 increaseNumber(i, j);
             }
@@ -97,7 +140,7 @@ function putBomb(sizeCamp, numberMines){
         }
     }
     contBomb();
-    for(let i = 0; i < size[0]**2; i++){
+    for(let i = 0; i < size[0] * size[1]; i++){
         tiles[i].children[0].textContent = camp[i];
     }
 }
@@ -106,6 +149,7 @@ function startGame(){
     let grid = this.firstChild.textContent.split('x');
     let columns = '';
     let rows = '';
+    let fontSize = '3rem';
     grid = Array.from(grid);
     let quantityMines = Array.from(this.lastChild.textContent).filter(number =>{
         if(number.match(/[0-9]/g)){
@@ -123,13 +167,22 @@ function startGame(){
     for(let i = 0; i < grid[1]; i++){
         rows += '1fr ';
     }
+    if(size[0] > size[1]){
+        container.style.width = '80vw';
+        fontSize = '1.5rem';
+    }
+    else if(size[0] == 16){
+        fontSize = '2rem';
+    }
     for(let i = 0; i < grid[0] * grid[1]; i++){
         camp.push(0);
         let tile = document.createElement('div');
         let para = document.createElement('p');
         tile.classList.add('tile');
         tile.appendChild(para);
-        tile.addEventListener('click', move);
+        tile.style.fontSize = fontSize;
+        tile.addEventListener('click', click);
+        tile.addEventListener('mousedown', putFlag);
         container.appendChild(tile);
     }
     container.style.gridTemplateColumns = columns;
